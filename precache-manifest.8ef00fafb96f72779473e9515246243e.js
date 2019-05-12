67a960f21e98de0e9aae6b4718642839b27c2517
Adarsha <adarsha69005@gmail.com>
2019-05-12 19:17:27 +0545

    Updates

---
+++ precache-manifest.8ef00fafb96f72779473e9515246243e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8983ada8c6db245c8fcc86f0d10482e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ea61b96d04d596ac89e5",
+    "url": "/portfolio/static/css/main.10b09600.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/portfolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ea61b96d04d596ac89e5",
+    "url": "/portfolio/static/js/main.55dc58d7.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
