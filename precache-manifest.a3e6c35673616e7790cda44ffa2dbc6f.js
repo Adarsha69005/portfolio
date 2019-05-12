17a679497b31f990c2442f9763daa1347f69149f
Adarsha <adarsha69005@gmail.com>
2019-05-12 22:46:26 +0545

    Updates

---
+++ precache-manifest.a3e6c35673616e7790cda44ffa2dbc6f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dbb25d0582ec1863df33239be43c840",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/portfolio/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c9353f4a914c80f8820c",
+    "url": "/portfolio/static/js/main.da62a328.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.fb35e6e10f1ee40147167db2fe9afe19.js"
+  "/portfolio/precache-manifest.a3e6c35673616e7790cda44ffa2dbc6f.js"
 );
 
 self.addEventListener('message', (event) => {
